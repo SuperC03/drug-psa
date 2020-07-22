dac24d658c46f964a7381082d99677a814ee9c39
SuperC <clarkbros3@gmail.com>
2020-07-22 10:50:48 +0200

    Fixed Images

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,8 @@
-import './styles.scss'
+import './styles.scss';
+
+// Use JS for Smooth Scrolling
+const navLinks = document.getElementsByClassName('nav-link');
+const dangers = document.getElementById('dangers');
+const example = document.getElementById('example');
+const sources = document.getElementById('examples');
+nav
