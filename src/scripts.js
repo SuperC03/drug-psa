7786266a2810e0bce702637c673140aaebdf1cc8
SuperC <clarkbros3@gmail.com>
2020-07-22 18:16:46 +0200

    Added Sources and Example

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,17 @@ import './styles.scss';
 // Use JS for Smooth Scrolling
 const navLinks = document.getElementsByClassName('nav-link');
 const dangers = document.getElementById('dangers');
+navLinks[0].addEventListener('click', (e) => {
+  e.preventDefault();
+  dangers.scrollIntoView({behavior: 'smooth'});
+});
 const example = document.getElementById('example');
-const sources = document.getElementById('examples');
-nav
+navLinks[1].addEventListener('click', (e) => {
+  e.preventDefault();
+  example.scrollIntoView({behavior: 'smooth'});
+});
+const sources = document.getElementById('sources');
+navLinks[2].addEventListener('click', (e) => {
+  e.preventDefault();
+  sources.scrollIntoView({behavior: 'smooth'});
+});
